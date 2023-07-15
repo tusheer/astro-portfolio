1e78b54189015fdab81f7696b0cf5494ad21a338
tusheer <jaanealamtusher@gmail.com>
2023-07-15 14:25:01 +0600

    feat: change profile links

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -14,19 +14,15 @@ const presentation: Presentation = {
   mail: "jaanealamtusher@gmail.com",
   title: "Hi, I’m Jane Alam 👋",
   description:
-    "Tusher, i'm a *bangladeshi frontend developer* with over *4 years* of web experience. I am currently working with *NextJS and Typescript*. Outside of work I play football and learning Threejs*.",
+    "Tusher, i'm a *Bangladeshi frontend developer* with over *4 years* of web experience. I am currently working with *NextJS and Typescript*. Outside of work I play football and learning *Threejs*.",
   socials: [
     {
       label: "Twiiter",
-      link: "https://twitter.com/itsstormzz_",
+      link: "https://twitter.com/tusheeer",
     },
-    {
-      label: "Bento",
-      link: "https://bento.me/m-wolff",
-    },
-    {
+        {
       label: "Github",
-      link: "https://github.com/MaeWolff",
+      link: "https://github.com/tusheer",
     },
   ],
 };
